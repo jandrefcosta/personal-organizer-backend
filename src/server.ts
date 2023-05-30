46fd6e5cbd1fe4c42aa92edbf0e3e9dbbb3c94d2
João André <jandrefcosta@gmail.com>
2023-05-30 16:59:26 -0300

    ajustes

--- src/server.ts
+++ src/server.ts
@@ -2,29 +2,29 @@ import 'dotenv/config'
 
 import fastify from 'fastify'
 import cors from '@fastify/cors'
-import jwt from '@fastify/jwt'
+// import jwt from '@fastify/jwt'
 
-import { authRoutes } from './routes/auth';
-import { usersRoutes } from './routes/users';
-import { expensesRoutes } from './routes/expenses';
-import { statusRoutes } from './routes/status';
-import { categoryRoutes } from './routes/category';
+// import { authRoutes } from './routes/auth';
+// import { usersRoutes } from './routes/users';
+// import { expensesRoutes } from './routes/expenses';
+// import { statusRoutes } from './routes/status';
+// import { categoryRoutes } from './routes/category';
 import { healthRoutes } from './routes/health';
 
 const app = fastify();
 
 app.register(cors, { origin: true })
-app.register(jwt, { secret: 'spacetime' })
+//app.register(jwt, { secret: 'spacetime' })
 
 app.register(healthRoutes)
 
-app.register(authRoutes)
-app.register(usersRoutes)
+// app.register(authRoutes)
+// app.register(usersRoutes)
 
-app.register(statusRoutes)
-app.register(categoryRoutes)
+// app.register(statusRoutes)
+// app.register(categoryRoutes)
 
-app.register(expensesRoutes)
+// app.register(expensesRoutes)
 
 
 app.listen({ port: 8080, }).then(() => {
